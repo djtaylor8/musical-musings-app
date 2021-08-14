e0826350aed75a8e67d8a8f816d3e482faf5d669
DJ Taylor <dtaylor88@gmail.com>
2021-08-13 22:42:42 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,14 @@ spotify_tracks.each do |song|
     )
 end
 
-User.create!(name: "Lou", email: "lou@me.com", password: "hi")
+lou = User.create!(name: "Lou", email: "lou@me.com", password: "hi")
+georgia = User.create!(name: "Georgia", email: "georgia@me.com", password: "hi")
+dolly = User.create!(name: "Dolly", email: "dolly@me.com", password: "hi")
+chance = User.create!(name: "Chance", email: "chance@me.com", password: "hi")
+kishi = User.create!(name: "Kishi", email: "kishi@me.com", password: "hi")
+
+play_1 = Playlist.create!(story: "test", user: lou)
+play_2 = Playlist.create!(story: "test", user: georgia)
+play_3 = Playlist.create!(story: "test", user: dolly)
+play_4 = Playlist.create!(story: "test", user: chance)
+play_5 = Playlist.create!(story: "test", user: kishi)
