a599107bf2235229eb36c3d73e036e8b124c4a7d
DJ Taylor <dtaylor88@gmail.com>
2021-08-15 01:40:44 -0700

    add show page for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,8 @@ class SongsController < ApplicationController
     def new
       @song = Song.new 
     end
+
+    def show 
+      @song = Song.find_by(id: params[:id])
+    end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -3,7 +3,7 @@ class Search < ApplicationRecord
     def find_songs(keywords)
       song_results = RSpotify::Track.search("#{keywords}")
       new_songs = song_results.first(7).map do |song|
-        Song.new(
+        Song.create!(
             name: song.name,
             artist: song.artists[0].name,
             genre: song.artists[0].genres[0],
