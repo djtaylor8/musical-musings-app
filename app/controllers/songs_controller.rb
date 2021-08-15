d352d41883e323d736fa2df96b5ce26907706dae
DJ Taylor <dtaylor88@gmail.com>
2021-08-15 11:00:31 -0700

    form to song to playlist on show page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,23 @@ class SongsController < ApplicationController
       @song = Song.new 
     end
 
+    def create 
+      @song = Song.find_by(params[:id]) 
+      @playlist = Playlist.find_by(title: params[:playlist][:title])
+      @playlist.songs << @song 
+      @playlist.save 
+      redirect_to playlist_path(@playlist)
+    end
+
     def show 
       @song = Song.find_by(id: params[:id])
+      @user = current_user 
+      @playlist = Playlist.all.find_by(user: current_user)
+    end
+
+    private 
+
+    def song_params
+      params.require(:song).permit(:name, :artist, :genre, :preview_url, :spotify_id, :title)
     end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,8 @@ class Song < ApplicationRecord
     has_many :playlist_songs
     has_many :playlists, through: :songs 
 
+    accepts_nested_attributes_for :playlists 
+
     def new_from_spotify(spotify_track)
       Song.create(
           title: spotify_track.name,
