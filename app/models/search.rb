eef6966ca5f77aebb4e751b2ada10477a2ebfac3
DJ Taylor <dtaylor88@gmail.com>
2021-08-20 18:49:54 -0700

    style user show page, increase search limit for songs

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,7 +4,7 @@ class Search < ApplicationRecord
 
     def find_songs(keywords)
       search_results = RSpotify::Track.search("#{keywords}")
-      songs = search_results.first(7).map do |song|
+      songs = search_results.first(20).map do |song|
         Song.find_or_create_by(
             name: song.name,
             artist: song.artists[0].name,
